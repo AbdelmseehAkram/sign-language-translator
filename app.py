7a7764cb28f2ebde84c469df6a100dd9d54b24d0
AbdelmseehAkram <abdelmseeha@gmail.com>
2025-02-15 02:25:54 +0200

    app.py

--- app.py
+++ app.py
@@ -1,30 +1,27 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
-import mediapipe as mp
+import subprocess
+subprocess.run(["pip", "install", "opencv-python-headless"])
+import cv2
 import numpy as np
-from PIL import Image, ImageDraw
+from cvzone.HandTrackingModule import HandDetector
 from keras.models import load_model
 import pyttsx3
+from string import ascii_uppercase
 import enchant
 import os
 
 # حل مشكلة libGL.so.1
 os.system("apt-get update && apt-get install -y libgl1-mesa-glx")
 
-# تحميل النموذج
+# إعداد المكونات الأساسية
 model = load_model('cnn8grps_rad1_model.h5')
-
+hd = HandDetector(maxHands=1)
+hd2 = HandDetector(maxHands=1)
 ddd = enchant.Dict("en-US")
 offset = 29
 engine = pyttsx3.init()
 engine.setProperty("rate", 100)
-
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands()
-
-def preprocess_image(image):
-    image = image.convert("L").resize((400, 400))  # تحويل لصورة رمادية وتغيير الحجم
-    return np.array(image).reshape(1, 400, 400, 1) / 255.0  # تحويل إلى مصفوفة وتطبيع القيم
 
 # حالة التطبيق
 if 'text' not in st.session_state:
@@ -34,26 +31,50 @@ if 'suggestions' not in st.session_state:
 
 # فئة معالجة الفيديو
 class SignLanguageTranslator(VideoTransformerBase):
+    def __init__(self):
+        self.white = np.ones((400, 400, 3), dtype=np.uint8) * 255
+
     def transform(self, frame):
-        img = frame.to_ndarray(format="rgb24")
-        results = hands.process(img)
+        img = frame.to_ndarray(format="bgr24")
+        img = cv2.flip(img, 1)
         
-        if results.multi_hand_landmarks:
-            img_pil = Image.fromarray(img)
-            draw = ImageDraw.Draw(img_pil)
+        # اكتشاف اليدين
+        hands = hd.findHands(img, draw=False, flipType=True)
+        
+        if hands:
+            hand = hands[0]
+            x, y, w, h = hand['bbox']
+            img_crop = img[y-offset:y+h+offset, x-offset:x+w+offset]
             
-            for hand_landmarks in results.multi_hand_landmarks:
-                points = [(int(lm.x * img.shape[1]), int(lm.y * img.shape[0])) for lm in hand_landmarks.landmark]
-                
-                connections = [(0, 4), (5, 8), (9, 12), (13, 16), (17, 20), (5, 9), (9, 13), (13, 17), (0, 5), (0, 17)]
-                for start, end in connections:
-                    draw.line([points[start], points[end]], fill=(0, 255, 0), width=3)
-            
-            white_input = preprocess_image(img_pil)
-            prediction = model.predict(white_input)[0]
-            ch1 = np.argmax(prediction)
-            st.session_state.text += chr(ch1 + 65)
-            
+            if img_crop.size != 0:
+                # معالجة الهيكل العظمي
+                handz = hd2.findHands(img_crop, draw=False, flipType=True)
+                if handz:
+                    hand = handz[0]
+                    pts = hand['lmList']
+                    os = ((400 - w) // 2) - 15
+                    os1 = ((400 - h) // 2) - 15
+                    
+                    # رسم الخطوط
+                    connections = [
+                        (0, 4), (5, 8), (9, 12), (13, 16), (17, 20),
+                        (5, 9), (9, 13), (13, 17), (0, 5), (0, 17)
+                    ]
+                    
+                    for start, end in connections:
+                        cv2.line(self.white, 
+                                (pts[start][0]+os, pts[start][1]+os1),
+                                (pts[end][0]+os, pts[end][1]+os1),
+                                (0, 255, 0), 3)
+                    
+                    # التنبؤ بالحرف
+                    white_input = cv2.resize(self.white, (400, 400))
+                    prediction = model.predict(np.array([white_input]))[0]
+                    ch1 = np.argmax(prediction)
+                    
+                    # إضافة الحرف للنص
+                    st.session_state.text += chr(ch1 + 65)
+                    
         return img
 
 # واجهة المستخدم
